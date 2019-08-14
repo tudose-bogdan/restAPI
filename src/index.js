b1b36b83eca0bddb8b146d63aef0045ff5d31fe2
tudose-bogdan <bogdan-cristian.tudose@my.fmi.unibuc.ro>
2019-08-14 14:35:54 +0300

    First

--- src/index.js
+++ src/index.js
@@ -1,36 +1,44 @@
-let express = require('express')
-let app = express()
-let personRoute = require('./routes/person')
-let accRoute = require('./routes/account')
-let path = require('path')
-let bodyParser = require('body-parser')
+let express = require('express');
+let app = express();
+let personRoute = require('./routes/person');
+let idRoute = require('./routes/id')
+let path = require('path');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
 
-app.use(bodyParser.json())
+mongoose.connect('mongodb://localhost:27017/t_data', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('connected')
+});
+
+app.use(bodyParser.json());
 
 app.use((req,res,next) =>{
-    console.log(`${new Date().toString()}  => ${req.originalUrl}`)
-
+    console.log(`${new Date().toString()}  => ${req.originalUrl}`);
     next()
-})
-app.use(personRoute)
-app.use(accRoute)
-app.use(express.static('public'))
+});
 
+app.use(personRoute);
+app.use(idRoute);
+app.use(express.static('public'));
 
 //handler for 404
 app.use((req,res,next)=>{
      res.status(404).send('Page not found')
-})
+});
 
 //handler for 500
 app.use((err,req,res,next) =>{
-    console.error(err.stack)
+    console.error(err.stack);
     res.sendFile(path.join(__dirname, '../public/500.html'))
-})
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.info(`server running on port ${PORT}`))
-
+app.listen(PORT, () => console.info(`server running on port ${PORT}`));
 
 
--- src/routes/id.js
+++ src/routes/id.js
@@ -2,21 +2,21 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
-mongoose.model('user', new mongoose.Schema({username:String, id:String}),'user_info');
+
 let user  = mongoose.model('user');
 
-router.get('/person', (req,res) => {
+router.get('/id', (req,res) => {
     if(req.query.name){
         console.log("searched for " + req.query.name);
-        user.find({username:`${req.query.name}`},(err,data) => {
-                try{
+        user.find({id:`${req.query.name}`},(err,data) => {
+            try{
                 user.find({id: data[0]['id']}, (err, data) => {
                     res.json(data)
                 })}
-                catch(err)
-                {
-                    res.status(404).send('Not found');
-                }
+            catch(err)
+            {
+                res.status(404).send('Not found');
+            }
         })
     }else
     {
@@ -24,20 +24,19 @@ router.get('/person', (req,res) => {
     }
 });
 
-router.get('/person/:name', (req,res) =>{
+router.get('/id/:name', (req,res) =>{
     if(req.params.name) {
         console.log("searched for " + req.params.name);
-        user.find({username: `${req.params.name}`}, (err, data) => {
-
-                try{
+        user.find({id: `${req.params.name}`}, (err, data) => {
+            try{
                 user.find({id: data[0]['id']}, (err, date) => {
                     res.json(date)
                 })}
-                catch(err)
-                {
-                    res.status(404).send('Not found');
+            catch(err)
+            {
+                res.status(404).send('Not found');
 
-                }
+            }
         })
     }
     else
